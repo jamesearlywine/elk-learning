ba25f9a62e4c6aa284d22c70d269cc9bbcec1214
James Earlywine <james@esilogix.com>
2024-09-29 20:47:33 -0400

    ..

--- fakeData/fakePerson.test.ts
+++ fakeData/fakePerson.test.ts
@@ -3,9 +3,7 @@ import { main } from "./fakePerson";
 describe("fakePerson.ts", () => {
   describe("main", () => {
     it("should do stuff", () => {
-      const processStdoutWriteSpy = jest.spyOn(process.stdout, "write");
       main();
-      expect(processStdoutWriteSpy).toHaveBeenCalled();
     });
   });
 });
